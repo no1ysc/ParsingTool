ced111e4e16fa72ce5be61f111a463629f3cb49e
JS <num1ysc@gmail.com>
2015-04-03 00:15:17 -0400

    1채 대상문서 가져오기 로직

---
+++ ParsingToolGUI/ParsingToolGUI/Logic/FindTargetDoc.cs
@@ -0,0 +1,207 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ParsingToolGUI.Logic
+{
+    class FindTargetDoc : BackgroundWorker
+    {
+        private class ResultSet
+        {
+            private string filePath, fileNamePDF;
+
+            public ResultSet(string filePath, string fileNamePDF)
+            {
+                this.fileNamePDF = fileNamePDF;
+                this.filePath = filePath;
+            }
+
+            public string FilePath { get { return filePath; } }
+            public string FileName { get { return fileNamePDF; } }
+
+            public override string ToString()
+            {
+                return filePath + "\t" + fileNamePDF;
+            }
+        }
+
+        private Queue<ResultSet> fileList = new Queue<ResultSet>();
+
+        // check words
+        private string[] checkWord = {
+        "무상급식",
+        "무\n상급식",
+        "무상\n급식",
+        "무상급\n식",
+        "무상 급식",
+        "무\n상 급식",
+        "무상\n 급식",
+        "무상 \n급식",
+        "무상 급\n식",
+        };
+
+
+
+
+        private string rootPath;
+        private string resultPath;
+
+        private MainWindow progressBarInWindow = null;
+
+        public FindTargetDoc(string rootPath, string resultPath)
+        {
+            this.rootPath = rootPath;
+            this.resultPath = resultPath;
+
+            this.DoWork += new DoWorkEventHandler(work);
+            this.RunWorkerCompleted += new RunWorkerCompletedEventHandler(completeWork);
+        }
+
+        public void Run()
+        {
+            // 폴더들 탐색하여 대상들 가져옴.
+            searchChildDirs(rootPath);
+
+            StreamWriter log = File.CreateText(Environment.CurrentDirectory + "\\Log.txt");
+
+            int totalTargetDocCount = fileList.Count;
+            int workingCount = 0;
+
+            log.WriteLine(fileList.Count + "개 파일 시작.");
+
+            // 대상들 처리
+            while(fileList.Count > 0)
+            {
+                workingCount++;
+
+                ResultSet item = fileList.Dequeue();
+
+                // PDF로부터 원문 가져옴.
+                // Getting rawText from PDF file.
+                string rawText = getRawText(item.FilePath + item.FileName);
+
+                // Checking a target word
+                if (containWordsCheck(rawText))
+                {
+                    // Copy to des.
+                    copyFileToResultDir(item);
+                    log.WriteLine(workingCount + "번째 파일 복사.");
+                }
+                else
+                {
+                    log.WriteLine(workingCount + "번째 파일 패스.");
+                }
+
+                this.ReportProgress( (int) ((double)totalTargetDocCount / (double)workingCount) * 100 );
+            }
+        }
+
+        /// <summary>
+        /// 프로그래스바 사용할껀지.
+        /// </summary>
+        public void UsingProgressBar(MainWindow progressBarInWindow)
+        {
+            this.progressBarInWindow = progressBarInWindow;
+            this.ProgressChanged += new ProgressChangedEventHandler(progressBarHandler);
+            this.WorkerReportsProgress = true;
+        }
+
+        private void work(object sender, DoWorkEventArgs e)
+        {
+            if (this.WorkerReportsProgress)
+            {
+                progressBarInWindow.ProgressBarVisablity = System.Windows.Visibility.Visible;
+                progressBarInWindow.ProgressBarValue = 0;
+            }
+
+            Run();
+        }
+
+        private void completeWork(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (this.WorkerReportsProgress)
+            {
+                progressBarInWindow.ProgressBarVisablity = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void progressBarHandler(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarInWindow.ProgressBarValue = e.ProgressPercentage;
+        }
+
+
+        private string getRawText(string pdfFilePath)
+        {
+            PdfReader reader = new PdfReader(pdfFilePath);
+
+            StringWriter output = new StringWriter();
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy()));
+
+            return output.ToString();
+        }
+
+        private void copyFileToResultDir(ResultSet item)
+        {
+            File.Copy(item.FilePath + item.FileName, resultPath + item.FileName);
+        }
+
+        private bool containWordsCheck(string text)
+        {
+            foreach (string word in checkWord)
+            {
+                if (text.Contains(word))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+
+        private void searchChildDirs(string startDir)
+        {
+            // 루트부터 전부 뒤짐.
+            // PDF 파일이름은 각 폴더에 한개씩만 물면됨.
+            // Text 는 이름만 만들어도 상관없음(경로의 합)
+
+            DirectoryInfo dirInfo = new DirectoryInfo(startDir);
+            bool isSub = dirInfo.EnumerateDirectories().Count() != 0 ? true : false;
+            if (!isSub)
+            {
+                // leaf 임.
+                string pdf = "", dir;
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    if (file.Extension.Contains("pdf"))
+                    {
+                        pdf = file.Name;
+                        dir = dirInfo.FullName + "\\";
+                        fileList.Enqueue(new ResultSet(dir, pdf));
+                    }
+                }
+
+                return;
+            }
+
+            // 하위 폴더 리커시브 탐색
+            foreach (DirectoryInfo subDir in dirInfo.EnumerateDirectories())
+            {
+                searchChildDirs(subDir.FullName);
+            }
+        }
+
+
+    }
+}
--- ParsingToolGUI/ParsingToolGUI/MainWindow.xaml.cs
+++ ParsingToolGUI/ParsingToolGUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ParsingToolGUI.Logic;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,54 @@ namespace ParsingToolGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string originPDFsDir = @"L:\Data\무상급식회의록\";
+        private string targetPDFsDir = @"L:\Data\무상급식회의록타켓";
+
+        private string outputJSONFilePath;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private delegate void DelProgressBarValue(int value);
+        private void progressBarValue(int value) { this.progressBar.Value = value; }
+        public int ProgressBarValue 
+        {
+            set
+            {
+                this.Dispatcher.Invoke(new DelProgressBarValue(progressBarValue), new object[] { value });
+            }
+        }
+
+        private delegate void DelProgressBarVisibility(System.Windows.Visibility value);
+        private void progressBarVisibility(System.Windows.Visibility value) { this.progressBar.Visibility = value; }
+        public System.Windows.Visibility ProgressBarVisablity
+        {
+            set
+            {
+                this.Dispatcher.Invoke(new DelProgressBarVisibility(progressBarVisibility), new object[] { value });
+            }
+        }
+        
+        // 넣기 버튼 클릭
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        // JSON 보기 클릭
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        // 대상 문서 선정 클릭.
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            FindTargetDoc findDoc = new FindTargetDoc(originPDFsDir, targetPDFsDir);
+            findDoc.UsingProgressBar(this);
+            findDoc.RunWorkerAsync();
         }
     }
 }
